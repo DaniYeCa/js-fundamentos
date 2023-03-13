7e22fcb82c9782134a478e0ad091ae3447b5d8cf
daniyeca <daniel.dance1912@gmail.com>
2023-03-13 12:35:03 +0100

    create variables session 01

---
+++ 01-variables.js
@@ -0,0 +1,29 @@
+var greettings = 'hola desde';
+var school = 'pmba';
+var greettingsFromSchool = greettings + ' ' + school;
+
+
+
+console.log(greettingsFromSchool);
+
+
+// holaQueTal (camelCase)
+// FINAL_TEXT (PASCAL_CASE)
+
+
+// string
+// number
+// boolean
+// Array
+// Object
+
+
+var ageAsString = '30'
+var ageAsNumber = 30
+
+
+var ArrayOfValues = [30, '30', false, true, 4]
+var ObjectOfValues = {
+    age: 30,
+    ageAsNumber: ageAsNumber
+}
